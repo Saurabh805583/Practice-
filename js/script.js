e2e66f324bc0a7d13a8b873878fdb844f61dd4f2
Saurabh805583 <saurabhingle9130@gmail.com>
2026-05-29 17:16:29 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+console.log("Dhanshre Portfolio Loaded");
+
+/* Typing Effect */
+
+const text = [
+"Future IT Professional",
+"Dreaming Big",
+"Tech Enthusiast",
+"Google • Microsoft • TCS Aspirations"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+
+letter = currentText.slice(0,++index);
+
+document.querySelector("h3").textContent = letter;
+
+if(letter.length === currentText.length){
+
+count++;
+
+index = 0;
+
+setTimeout(type,1500);
+
+}else{
+
+setTimeout(type,100);
+
+}
+
+})();
